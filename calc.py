37912e8322b96093d0546aa69bc50758d7997e29
Paulslewis66 <paul.s66@gmail.com>
2024-09-29 00:46:09 +0100

    version 2

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+from operator import sub
+from functools import reduce
+
 print("Please Select a Mathemat Operation")
 print("=======================")
 print("1 - Add number ")
@@ -14,37 +17,41 @@ def showmenu():
         menu_option = input(choice)
         if menu_option == '1':
             print("You have Chosen Add Numbers")
-            a = 0
-            b = 0
-            a = input("Please Type a number > ")
-            b = input("please type a second number > ")
-            sum = int(a) + int(b)
-            print("Answer is", sum)
-     
+            numbers = input("Enter numbers separated by spaces and normal plus signs: ")
+            numbers_list = numbers.split()
+            numbers_list = [char for char in numbers_list if char != '+']
+            numbers_list = [int(num) for num in numbers_list]
+            total_sum = sum(numbers_list)
+            print("the ansswe is", total_sum)
+            
         elif menu_option == '2':
             print("You have chosen Subtract Numbers")
-            a = 0
-            b= 0
-            a = input("Please Type a number > ")
-            b = input("please type a second number > ")
-            sum = int(a) - int(b)
-            print("Answer is", sum)
+            numbers = input("Enter numbers with spaces and minus signs : ")
+            numbers_list = numbers.split()
+            numbers_list = [char for char in numbers_list if char != '-']
+            numbers_list = [int(num) for num in numbers_list]
+            total_sum = reduce(sub, numbers_list)
+            print("the ansswe is", total_sum)
 
         elif menu_option == '3':
             print("You Have Chosen Multiply Numbers")
-            a = 0
-            b = 0
-            a = input("Please Type a number > ")
-            b = input("please type a second number > ")
-            sum = int(a) * int(b)
-            print("Answer is", sum) 
+            numbers = input("Enter numbers with spaces and times signs : ")
+            numbers_list = numbers.split()
+            numbers_list = [char for char in numbers_list if char != 'x']
+            numbers_list = [int(num) for num in numbers_list]
+            result = 1
+            for i in numbers_list:
+                result *= i
+            print("the ansswe is", result)
 
         elif menu_option == '4':
             print("You Have Chosen Divide Numbers")
-            a = 0
-            b = 0
-            a = input("Please Type a number > ")
-            b = input("please type a second number > ")
-            sum = int(a) / int(b)
-            print("Answer is", sum)
+            numbers = input("Enter numbers with spaces and times signs : ")
+            numbers_list = numbers.split()
+            numbers_list = [char for char in numbers_list if char != '/']
+            numbers_list = [int(num) for num in numbers_list]
+            result = 1
+            for i in numbers_list:
+                result /= i
+            print("the ansswe is", result)
 showmenu()
