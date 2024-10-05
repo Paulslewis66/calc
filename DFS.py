2b4a6959b97da749b8e03f919b26cf45fb0aa5cc
paulslewis66 <paul.s66@gmail.com>
2024-10-05 15:25:49 +0100

    BFS

---
+++ DFS.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+G = {
+    '0': ['7', '5', '3'],
+    '7': ['2', '4'],
+    '5': ['1'],
+    '3': ['6'],
+}
+
+def searchBFS(G, root):
+    checked = set()
+    input(root)
+    
+
+searchBFS(G, '2')
