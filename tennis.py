aeb1b69a1342045ecb43c98b6221aeaa64f131c0
Paulslewis66 <paul.s66@gmail.com>
2024-10-12 20:44:25 +0100

    c

--- tennis.py
+++ tennis.py
@@ -1,45 +1,33 @@
 import pandas as pd
 
-df = pd.read_csv('tennis_data.txt',header=None)
-df.columns = ['ID','outlook', 'temp', 'humidity', 'wind', 'play']
+df = pd.read_csv('tennis_data.txt',header=None) # Read in Tennis.CSV with observations
+df.columns = ['ID','outlook', 'temp', 'humidity', 'wind', 'play'] # Add Column Names
 
-hot_no = len(df[(df['temp']=='Hot') & (df['play']=='No')]) / len(df[df['play']=='No'])
-mild_no = len(df[(df['temp']=='Mild') & (df['play']=='No')]) / len(df[df['play']=='No'])
-cool_no = len(df[(df['temp']=='Cool') & (df['play']=='No')]) / len(df[df['play']=='No'])
+hot_no = len(df[(df['temp']=='Hot') & (df['play']=='No')]) / len(df[df['play']=='No']) # calculate Likelihood of Hot Tennis given P(B|A) 
+mild_no = len(df[(df['temp']=='Mild') & (df['play']=='No')]) / len(df[df['play']=='No']) # Calc likelihood of Mile Tennis give P(B|A)
+cool_no = len(df[(df['temp']=='Cool') & (df['play']=='No')]) / len(df[df['play']=='No']) # ibid...
 hot_yes = len(df[(df['temp']=='Hot') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 mild_yes = len(df[(df['temp']=='Mild') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 cool_yes = len(df[(df['temp']=='Cool') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 
-print("Prob Temp Hot, Prior No - ", hot_no, "\nProb Temp Hot, Prior Yes - ", round(hot_yes,1), "\nProb Temp Mild, Prior No - ", 
-mild_no, "\nProb Temp Mild, Prior Yes - ", round(mild_yes,1), "\nProb Temp Cool, Prior No - ", cool_no, "\nProb Temp Cool, Prior Yes - ", 
-round(cool_yes,1))
-
-sunny_no = len(df[(df['outlook']=='Sunny') & (df['play']=='No')]) / len(df[df['play']=='No'])
+sunny_no = len(df[(df['outlook']=='Sunny') & (df['play']=='No')]) / len(df[df['play']=='No']) 
 overcast_no = len(df[(df['outlook']=='Overcast') & (df['play']=='No')]) / len(df[df['play']=='No'])
 rain_no = len(df[(df['outlook']=='Rain') & (df['play']=='No')]) / len(df[df['play']=='No'])
 sunny_yes = len(df[(df['outlook']=='Sunny') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 overcast_yes = len(df[(df['outlook']=='Overcast') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 rain_yes = len(df[(df['outlook']=='Rain') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 
-####print(sunny_no, overcast_no, rain_no, sunny_yes, overcast_yes, rain_yes)
-
 strong_no = len(df[(df['wind']=='Strong') & (df['play']=='No')]) / len(df[df['play']=='No'])
 weak_no = len(df[(df['wind']=='Weak') & (df['play']=='No')]) / len(df[df['play']=='No'])
 strong_yes = len(df[(df['wind']=='Strong') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 weak_yes = len(df[(df['wind']=='Weak') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
-
-##print(strong_no, weak_no, strong_yes, strong_no)
 
 high_no = len(df[(df['humidity']=='High') & (df['play']=='No')]) / len(df[df['play']=='No'])
 normal_no = len(df[(df['humidity']=='Normal') & (df['play']=='No')]) / len(df[df['play']=='No'])
 high_yes = len(df[(df['humidity']=='High') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 normal_yes = len(df[(df['humidity']=='Normal') & (df['play']=='Yes')]) / len(df[df['play']=='Yes'])
 
-#print(high_no, high_yes, normal_no, normal_yes)
+print("Prob Temp Hot, Prior No - ", hot_no, "\nProb Temp Hot, Prior Yes - ", round(hot_yes,1), "\nProb Temp Mild, Prior No - ", 
+mild_no, "\nProb Temp Mild, Prior Yes - ", round(mild_yes,1), "\nProb Temp Cool, Prior No - ", cool_no, "\nProb Temp Cool, Prior Yes - ", 
+round(cool_yes,1))
 
-result = rain_yes * len(df[df['play']=='Yes']) / len(df[df['play']=='No'])
-print(" If it rains will i play Tennis? =", result)
-result = sunny_yes * len(df[df['play']=='Yes']) / len(df[df['play']=='No'])
-print(" If it sunny will i play Tennis? =", result)
-result = weak_yes * len(df[df['play']=='Yes']) / len(df[df['play']=='No'])
-print(" If it strong wind will i play Tennis? =", result)
